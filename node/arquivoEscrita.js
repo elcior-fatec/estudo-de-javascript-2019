c4ccc57e5222c8c3bd022cb6a97c73e1f0feccb6
elcior.carvalho <elcior.carvalho@comlink.com.br>
2019-08-19 17:25:34 -0300

    Aulas de I/O com Node

--- node/arquivoEscrita.js
+++ node/arquivoEscrita.js
@@ -6,6 +6,11 @@ const produto = {
 	desconto: 0.15
 }
 
+
+// O metodo fs.writeFile() recebe 3 parametros:
+// 		- Caminho para o arquivo, incluindo seu nome
+// 		- O conteúdo a ser escrito no arquivo
+// 		- A função callback para tratar os erros de escrita
 fs.writeFile(__dirname + '/arquivoGerado.json', JSON.stringify(produto), err => {
 	console.log(err || "Arquivo salvo!")
 })
