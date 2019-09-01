3d6e55051a20f4cd8973001bc379386cd0fea81f
elcior-fatec <elciorfscarvalho@gmail.com>
2019-09-01 16:20:35 -0300

    HTML: forms

---
+++ html/exercicios/form/server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+// A constante app será o servidor express
+const app = express()
+const bodyParser = require('body-parser')
+
+// Faz o parser do body-parser, sem este parser a aplicação não consegue ler o conteúdo do body da request
+// O body-parser també, organiza as informações do body da requisição em uma estrutura de objeto pronta para ser processada pelo javascript
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// Definimos que o servidor irá processar as requisições do tipo POST vidas da URL /usuarios da maneira definida na callback
+app.get('/usuarios', (req, resp) => {
+    // imprime o body da requisição
+    console.log(req.body)
+    console.log(req.query)
+    // envia na resposta o html a baixo
+    resp.send('<h1>Parabéns! Usuário incluido!!!</h1>')
+})
+
+app.post('/usuarios/:id', (req, res) => {
+    console.log(req.params.id)
+    console.log(req.body)
+    res.send('<h1>Parabéns! Usuários alterado!!!</h1>')
+})
+
+// usar a porta 3003 para os processos inciados neste server.js
+app.listen(3003)
