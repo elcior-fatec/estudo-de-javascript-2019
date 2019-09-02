c41e35a8d4bc2fabb98b2f54f9647be0f7371092
elcior-fatec <elciorfscarvalho@gmail.com>
2019-09-02 00:19:00 -0300

    HTML: Imagens

--- html/exercicios/form/server.js
+++ html/exercicios/form/server.js
@@ -7,16 +7,19 @@ const bodyParser = require('body-parser')
 // O body-parser també, organiza as informações do body da requisição em uma estrutura de objeto pronta para ser processada pelo javascript
 app.use(bodyParser.urlencoded({ extended: true }))
 
-// Definimos que o servidor irá processar as requisições do tipo POST vidas da URL /usuarios da maneira definida na callback
+// Definimos que o servidor irá processar as requisições do tipo GET vidas da URL /usuarios da maneira definida na callback
 app.get('/usuarios', (req, resp) => {
-    // imprime o body da requisição
+    // imprime os parametros do body da requisição para POST
     console.log(req.body)
+    // imprime os parametros do body da requisição para GET
     console.log(req.query)
     // envia na resposta o html a baixo
     resp.send('<h1>Parabéns! Usuário incluido!!!</h1>')
 })
 
 app.post('/usuarios/:id', (req, res) => {
+    // a cima informamos que o ultimo parametro da URL será o 'id'
+    // a baixo acessamos o parametro :id da URL
     console.log(req.params.id)
     console.log(req.body)
     res.send('<h1>Parabéns! Usuários alterado!!!</h1>')
